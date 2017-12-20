64e8ee1a05da9967e7f37dae0e4c89c0398c91a8
Sneha-P-P <sneha6ps@gmail.com>
2017-12-20 16:01:27 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<div><h1>{{pageTitle}}</h1>
-  <div>My first component</div></div>`,
-  // styleUrls: ['./app.component.css']
+  <app-product-list></app-product-list></div>`,
+  // styleUrls: [' ./app.component.css']
 })
 export class AppComponent {
-  pageTitle = "ACME Product Management"
+  pageTitle: string = "ACME Product Management"
 ;}
